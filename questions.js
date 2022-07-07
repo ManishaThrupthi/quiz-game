db18343a891b9ddc6f52a820b1181fcecb67a624
Manithrupthi <92638725+Manithrupthi@users.noreply.github.com>
2022-07-07 07:49:42 +0530

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+let questions = [
+
+  {
+    numb : 1,
+    question: "Which one of the below is not divide and conquer approach?",
+    answer: "Merge sort",
+    options : [
+        "Insertion sort",
+        "Merge sort",
+        "Shell sort",
+        "Heap sort",
+     ]
+  },
+  {
+    numb : 2,
+    question: "Stack is used for?",
+    answer: "Recursion",
+    options : [
+    "CPU Resource Allocation",
+    "Breadth First Traversal",
+    "Recursion",
+    "None of the above",
+     ]
+  },
+  {
+    numb : 3,
+    question: "Quick sort algorithm is an example of",
+    answer: "Divide and Conquer",
+    options : [
+        "Greedy approach",
+        "Improved binary search",
+        "Dynamic Programming",
+        "Divide and Conquer",
+     ]
+  },
+  {
+    numb : 4,
+    question: "Maximum degree of any vertex in a simple graph of vertices n is",
+    answer: "n-1",
+    options : [
+        "2*n-1",
+        "n",
+        "n+1",
+        "n-1",
+     ]
+  },
+  {
+    numb : 5,
+    question: " Which of the below given series is Non-Increasing Order −",
+    answer: "9,8,6,3,3,1",
+    options : [
+        "1,3,4,6,8,9",
+        "9,8,6,4,3,1",
+        "9,8,6,3,3,1",
+        "1,3,3,6,8,9",
+     ]
+  },
+
+];
